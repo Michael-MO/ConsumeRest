e7c0094525c79e6b9111dc11e1b1972b716ba611
Michael-MO <Michael-MO@users.noreply.github.com>
2018-09-21 13:35:45 +0200

    Moved gitignore and other git files around

---
+++ ConsumeRest/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace ConsumeRest
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            RestConsumer consumer = new RestConsumer();
+            consumer.Start();
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ ConsumeRest/RestConsumer.cs
@@ -0,0 +1,45 @@
+﻿using ModelLib;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Net.Http;
+
+namespace ConsumeRest
+{
+    public class RestConsumer
+    {
+        private const string URI = "https://jsonplaceholder.typicode.com/todos";
+
+        public RestConsumer() { }
+
+        public void Start()
+        {
+            var restDatas = GetAll();
+            
+            foreach (RestData data in restDatas)
+            {
+                Console.WriteLine(data);
+            }
+        }
+
+        private IList<RestData> GetAll()
+        {
+            using (HttpClient client = new HttpClient())
+            {
+                string content = client.GetStringAsync(URI).Result;
+                IList<RestData> cList = JsonConvert.DeserializeObject<IList<RestData>>(content);
+                return cList;
+            }
+        }
+
+        private RestData GetOne(int id)
+        {
+            using (HttpClient client = new HttpClient())
+            {
+                string content = client.GetStringAsync(URI).Result;
+                RestData obj = JsonConvert.DeserializeObject<RestData>(content);
+                return obj;
+            }
+        }
+    }
+}
